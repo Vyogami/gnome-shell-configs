8e8b20e7877eba993978f075818c5645c8fc390f
legitShivam <shiv.05cs@gmail.com>
2023-05-16 01:43:59 +0530

    feat(script): add settings saving functionality

--- settings-manager.sh
+++ settings-manager.sh
@@ -18,6 +18,19 @@ load_gnome_settings() {
     echo "GNOME settings loaded successfully."
 }
 
+# Function to save current GNOME settings
+save_gnome_settings() {
+    read -p "Enter the name of the GNOME settings file to save (or leave blank for default): " gnome_settings_file
+
+    if [[ -z "$gnome_settings_file" ]]; then
+        gnome_settings_file="gnome-settings.ini"
+    fi
+
+    echo "Saving current GNOME settings..."
+    dconf dump / > "$gnome_settings_file"
+    echo "GNOME settings saved successfully."
+}
+
 # Function to load extension settings
 load_extension_settings() {
     read -p "Enter the name of the extension settings file (or leave blank for default): " extension_settings_file
@@ -36,14 +49,29 @@ load_extension_settings() {
     echo "Extension settings loaded successfully."
 }
 
+# Function to save current extension settings
+save_extension_settings() {
+    read -p "Enter the name of the extension settings file to save (or leave blank for default): " extension_settings_file
+
+    if [[ -z "$extension_settings_file" ]]; then
+        extension_settings_file="extension-settings.ini"
+    fi
+
+    echo "Saving current extension settings..."
+    dconf dump /org/gnome/shell/extensions/ > "$extension_settings_file"
+    echo "Extension settings saved successfully."
+}
+
 # Main program
 echo "GNOME and Extension Settings Loader"
 
 while true; do
     echo "Menu:"
     echo "1. Load GNOME settings"
-    echo "2. Load extension settings"
-    echo "3. Quit"
+    echo "2. Save current GNOME settings"
+    echo "3. Load extension settings"
+    echo "4. Save current extension settings"
+    echo "5. Quit"
 
     read -p "Enter your choice: " choice
 
@@ -52,9 +80,15 @@ while true; do
             load_gnome_settings
             ;;
         2)
-            load_extension_settings
+            save_gnome_settings
             ;;
         3)
+            load_extension_settings
+            ;;
+        4)
+            save_extension_settings
+            ;;
+        5)
             echo "Exiting program."
             break
             ;;
@@ -65,4 +99,3 @@ while true; do
 
     echo
 done
-
